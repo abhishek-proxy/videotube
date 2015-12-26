ba729ee082f2ddc52d2d359171bcabc63fc0c113
Abhishek Sangwan <Linux@Abhisheks-MacBook-Pro.local>
2015-12-27 00:39:10 +0530

    this is an application that is used to test the youtube player api

---
+++ app/src/main/java/com/example/linux/videotube/Constants.java
@@ -0,0 +1,9 @@
+package com.example.linux.videotube;
+
+/**
+ * Created by Linux on 12/26/15.
+ */
+public final class Constants {
+    public static final String DEVELOPER_KEY = "AIzaSyCHBu5hTjUf1Au2eSmFeZ3vaavwvoEdoN0";
+    public static final String YOUTUBE_VIDEO_CODE = "QOiSXE8P0zM";
+}
